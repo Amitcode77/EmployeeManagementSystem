77ec0f572163db1c007dfd4a0a386e12dcd0a0aa
Amit <amitcode77@gmail.com>
2022-11-09 03:20:55 +0530

    Database Change

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, g
-from database import get_database
+from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
-import sqlite3
+from database import app, db, users, emp
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(24)
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 @app.teardown_appcontext
@@ -14,37 +14,44 @@ def close_database(error):
         g.employeeapplication.close()
 
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 def get_current_user():
     user = None
     if 'user' in session:
         user = session['user']
-        db = get_database()
-        user_cur = db.execute('select * from users where name = ?', [user])
-        user = user_cur.fetchone()
+        user_cur = users.query.filter_by(name=user)
+        user = user_cur.first()
     return user
 
 
 @app.route('/')
 def index():
     user = get_current_user()
-    return render_template('index.html', user=user)
+    max_projects = emp.query.order_by(emp.total_projects).first()
+    max_testcase = emp.query.order_by(emp.total_test_case).first()
+    max_bughunter = emp.query.order_by(emp.total_defect_found).first()
+
+    return render_template('index.html', user=user, max_projects=max_projects, max_testcase=max_testcase, max_bughunter=max_bughunter)
 
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     user = get_current_user()
     error = None
-    db = get_database()
 
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        user_cursor = db.execute('select * from users where name = ?', [name])
-        user = user_cursor.fetchone()
+        user_cursor = users.query.filter_by(name=name)
+        user = user_cursor.first()
 
         if user:
-            if check_password_hash(user['password'], password):
-                session['user'] = user['name']
+            if check_password_hash(user.password, password):
+                session['user'] = user.name
                 return redirect(url_for('dashboard'))
             else:
                 error = "Username or Password did not match"
@@ -57,21 +64,27 @@ def login():
 @app.route('/register', methods=["POST", "GET"])
 def register():
     user = get_current_user()
-    db = get_database()
 
     if request.method == 'POST':
         name = request.form['name']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password)
+        password1 = request.form['password1']
+        password2 = request.form['password2']
+        if password1 != password2:
+            return render_template('register.html', registererror="Confirm Password Correctly!")
 
-        registered_users = db.execute('select * from users where name = ?', [name])
-        existing_user = registered_users.fetchone()
+        hashed_password = generate_password_hash(password1)
+
+        existing_user = users.query.filter_by(name=name).first()
+        # existing_user = registered_users.fetchone()
 
         if existing_user:
             return render_template('register.html', registererror="Username already taken, try different username")
 
-        db.execute('insert into users (name,password) values (?,?)', [name, hashed_password])
-        db.commit()
+        # db.execute('insert into users (name,password) values (?,?)', [name, hashed_password])
+        # db.commit()
+        new_user = users(name=name, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
         return redirect(url_for('index'))
 
     return render_template('register.html', user=user)
@@ -80,9 +93,8 @@ def register():
 @app.route('/dashboard')
 def dashboard():
     user = get_current_user()
-    db = get_database()
-    emp_cursor = db.execute('select * from emp')
-    all_emp = emp_cursor.fetchall()
+
+    all_emp = emp.query.all()
     return render_template('dashboard.html', user=user, all_emp=all_emp)
 
 
@@ -95,9 +107,14 @@ def addnewemp():
         phone = request.form['phone']
         address = request.form['address']
 
-        db = get_database()
-        db.execute('insert into emp(name,email,phone,address) values (?,?,?,?)', [name, email, phone, address])
-        db.commit()
+        new_emp = emp(name=name, email=email, phone=phone, address=address)
+        db.session.add(new_emp)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+        # db = get_database()
+        # db.execute('insert into emp(name,email,phone,address) values (?,?,?,?)', [name, email, phone, address])
+        # db.commit()
         return redirect(url_for('dashboard'))
     return render_template('addnewemployee.html', user=user)
 
@@ -105,18 +122,16 @@ def addnewemp():
 @app.route('/singleemp/<int:empid>')
 def singleemp(empid):
     user = get_current_user()
-    db = get_database()
-    emp_cur = db.execute("select * from emp where empid=?", [empid])
-    single_emp = emp_cur.fetchone()
+    emp_cur = emp.query.filter_by(empid=empid)
+    single_emp = emp_cur.first()
     return render_template('singleemployeeprofile.html', user=user, single_emp=single_emp)
 
 
 @app.route('/fetchone/<int:empid>')
 def fetchone(empid):
     user = get_current_user()
-    db = get_database()
-    emp_cur = db.execute("select * from emp where empid=?", [empid])
-    single_emp = emp_cur.fetchone()
+    emp_cur = emp.query.filter_by(empid=empid)
+    single_emp = emp_cur.first()
     return render_template('updateemployee.html', user=user, single_emp=single_emp)
 
 
@@ -133,11 +148,19 @@ def update():
         total_test_case = request.form['total_test_case']
         total_defect_found = request.form['total_defect_found']
         total_defects_pending = request.form['total_defects_pending']
-        db = get_database()
-        db.execute('update emp set name=?,email=?,phone=?,address=?,total_projects=?,total_test_case=?,'
-                   'total_defect_found=?,total_defects_pending=? where empid=?',
-                   [name, email, phone, address, total_projects, total_test_case, total_defect_found, total_defects_pending, empid])
-        db.commit()
+        update_emp = emp(name=name, email=email, phone=phone, address=address, total_projects=total_projects,
+                         total_test_case=total_test_case, total_defect_found=total_defect_found,
+                         total_defects_pending=total_defects_pending)
+        db.session.add(update_emp)
+        db.session.commit()
+        return redirect(url_for('index'))
+        # db = get_database()
+        # db.execute('update emp set name=?,email=?,phone=?,address=?,total_projects=?,total_test_case=?,'
+        #            'total_defect_found=?,total_defects_pending=? where empid=?',
+        #            [name, email, phone, address, total_projects, total_test_case, total_defect_found,
+        #             total_defects_pending, empid])
+        # db.commit()
+
         return redirect(url_for('dashboard'))
     return render_template('updateemployee.html', user=user)
 
@@ -146,9 +169,9 @@ def update():
 def deleteemp(empid):
     user = get_current_user()
     if request.method == "GET":
-        db = get_database()
-        emp_cursor = db.execute('delete from emp where empid = ?', [empid])
-        db.commit()
+        data = emp.query.get(empid)
+        db.session.delete(data)
+        db.session.commit()
         return redirect(url_for('dashboard'))
     return render_template('dashboard.html', user=user)
 
--- database.py
+++ database.py
@@ -1,15 +1,50 @@
-from flask import g
-import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, session, g
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///empmanagement.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-def connect_to_database():
-    sql = sqlite3.connect('C:/Users/ASUS/PycharmProjects/EMS/employeeapplication.db')
-    sql.row_factory = sqlite3.Row
-    return sql
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
+    admin = db.Column(db.Boolean, unique=False)
+
+    def __init__(self, name, password, admin=0):
+        self.name = name
+        self.password = password
+        self.admin = admin
 
 
-def get_database():
-    if not hasattr(g, 'employeeapplication'):
-        g.employeeapplication = connect_to_database()
+class emp(db.Model):
+    empid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    email = db.Column(db.String(20), unique=False, nullable=False)
+    phone = db.Column(db.Integer, unique=False, nullable=False)
+    address = db.Column(db.String(100), unique=False, nullable=False)
+    joining_date = db.Column(db.Date, unique=False, nullable=False)
+    total_projects = db.Column(db.Integer, unique=False, nullable=False)
+    total_test_case = db.Column(db.Integer, unique=False, nullable=False)
+    total_defect_found = db.Column(db.Integer, unique=False, nullable=False)
+    total_defects_pending = db.Column(db.Integer, unique=False, nullable=False)
 
-    return g.employeeapplication
+    def __init__(self, name, email, phone, address, total_projects=1, total_test_case=5,
+                 total_defect_found=1, total_defects_pending=1):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.joining_date = date.today()
+        self.total_projects = total_projects
+        self.total_test_case = total_test_case
+        self.total_defect_found = total_defect_found
+        self.total_defects_pending = total_defects_pending
